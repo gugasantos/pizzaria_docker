b44cb2d0c6f77d34190e544f3be1db1c52a8a710
Gustavo Santos <gustav0.0ggs@gmail.com>
2023-07-03 15:12:24 -0300

    Colocado bastante coisa sobre os graficos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Client;
 use App\Models\Pedido;
 use App\Models\Pizzas;
+use App\Models\PizzasPedido;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -12,22 +13,43 @@ class HomeController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
+
+        $day = $request->interval ? $request->interval:30;
+        $interval = intval($request->input('interval',30));
+        $datelimit = date('Y-m-d H:i:s', strtotime('-'.$interval. 'days'));
+
         $pizza = Pizzas::all();
         $cliente = Client::all();
         $pedido = Pedido::all();
 
         $price = array();
-        foreach($pedido as $p){
+
+        $lucrocount = (pedido::all()->where('created_at','>=',$datelimit));
+        foreach($lucrocount as $p){
             array_push($price, $p->price);
         }
+
+        $pagePie = [];
+        $pizzascount = PizzasPedido::selectRaw('pizzas_pedido_id, count(pizzas_pedido_id) as p')->groupBy('pizzas_pedido_id')->get();
+        foreach($pizzascount as $pizzacount){
+            $pagePie[ $pizzacount['pizzas_pedido_id']] = intval($pizzacount['p']);
+
+        };
+
+        $pageLabels = json_encode( array_keys($pagePie));
+        $pageValues = json_encode( array_values($pagePie));
 
         return view('actions.home',[
             'pizzas' => $pizza->count(),
             'clientes' => $cliente->count(),
-            'pedidos' => $pedido->count(),
-            'price' => array_sum($price)
+            'pedidos' => $pedido->where('created_at','>=',$datelimit)->count(),
+            'price' => array_sum($price),
+            'pageLabels' =>  $pageLabels,
+            'pageValues' =>  $pageValues,
+            'dataInterval' => $interval,
+            'days' => $day
 
         ]
         );
--- resources/views/actions/home.blade.php
+++ resources/views/actions/home.blade.php
@@ -1,14 +1,28 @@
 @extends('adminlte::page')
 
+@section('plugins.Chartjs', true)
+
 @section('title', 'Painel')
 
-@section('content_header')
-    <h1>Dashboard</h1>
-
-@endsection
-
-
 @section('content')
+<div class="row">
+    <div class="col-md-6 ">
+        <h1>Dashboard</h1>
+    </div>
+
+    <div class="col-md-6">
+        <form action="{{route('dashboard.index')}}" method="GET" class="form-horizontal">
+            <div class="float-md-right" style="display: flex">
+                <h5>Intervalo de dias:</h5>
+                <input type="number" name="interval" value="{{$days}}" class="form-control" style="margin: 0 15px; width:5rem">
+                <input type="submit" value="Calcular" class="btn btn-danger">
+            </div>
+        </form>
+
+    </div>
+
+
+</div>
     <div class="row">
         <div class="col-md-3">
             <div class="small-box bg-info">
@@ -60,17 +74,17 @@
         <div class="col-md-6">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">teste</h3>
+                    <h2 class="card-title">Pizzas mais compradas</h2>
                 </div>
                 <div class="card-body">
-                    ...
+                    <canvas id="pagePie">teste</canvas>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Sobre a pizzaria</h3>
+                    <h2 class="card-title">Clientes mais fiéis</h2>
                 </div>
                 <div class="card-body">
                     ...
@@ -78,5 +92,27 @@
             </div>
         </div>
     </div>
+
+<script>
+    window.onload = function(){
+        let ctx = document.getElementById('pagePie').getContext('2d');
+        window.pagePie = new Chart(ctx, {
+            type:'pie',
+            data:{
+                datasets:[{
+                    data:{{$pageValues}},
+                    backgroundColor: '#0000FF'
+                }],
+                labels:{!!$pageLabels!!}
+            },
+            options:{
+                responsive:true,
+                legend:{
+                    display:false
+                }
+            }
+        });
+    }
+</script>
 
 @endsection
