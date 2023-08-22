30c36c726dcea933b75ec8451f5912c9f10658af
Gustavo Santos <gustavo.santos.terceirizado@icmbio.gov.br>
2023-08-22 14:01:37 -0300

    Alterado algumas traducoes

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="https://raw.githubusercontent.com/gugasantos/pizzaria_docker/main/public/vendor/adminlte/dist/img/AdminLTELogoMarinara-removebg.png">
+<img width="150px" height="150px" src="https://raw.githubusercontent.com/gugasantos/pizzaria_docker/main/public/vendor/adminlte/dist/img/Logo2Marinara-removebg.png">
